d8e149f5f6ab655d82cf002c9efa22018bfc049f
Matt Miller <112419266+mkmiller6@users.noreply.github.com>
2024-06-06 12:04:24 -0500

    chore: update attendanceToTestout events (#13)

--- attendanceToTestout.py
+++ attendanceToTestout.py
@@ -10,23 +10,25 @@ from helpers.api import apiCall
 from config import N_APIkey, N_APIuser
 
 # Neon Account Info
-N_auth = f'{N_APIuser}:{N_APIkey}'
-N_baseURL = 'https://api.neoncrm.com/v2'
+N_auth = f"{N_APIuser}:{N_APIkey}"
+N_baseURL = "https://api.neoncrm.com/v2"
 N_signature = base64.b64encode(bytearray(N_auth.encode())).decode()
-N_headers = {'Content-Type': 'application/json',
-             'Authorization': f'Basic {N_signature}'}
+N_headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Basic {N_signature}",
+}
 
 today = datetime.date.today()
-deltaDays = today - datetime.timedelta(days = 7)
+deltaDays = today - datetime.timedelta(days=7)
 
 logging.basicConfig(
-    format = '%(asctime)s %(levelname)-8s %(message)s',
-    level = logging.INFO,
-    datefmt= '%Y-%m-%d %H:%H:%S'
+    format="%(asctime)s %(levelname)-8s %(message)s",
+    level=logging.INFO,
+    datefmt="%Y-%m-%d %H:%H:%S",
 )
 
 EVENT_FIELDS = {
-    "Specialty Tools": "440",
+    "Festool Domino": "440",
     "Shaper Origin": "274",
     "Woodshop Safety": "84",
     "Metal Shop Safety": "520",
@@ -38,6 +40,7 @@ EVENT_FIELDS = {
     "MIG Welding": "678",
     "TIG Welding": "679",
     "Small Lasers": "680",
+    "Laser Engrave Round": "1007",
     "Milling": "681",
     "Tormach": "682",
     "Filament": "683",
@@ -47,28 +50,33 @@ EVENT_FIELDS = {
     "Orientation": "182",
 }
 
+
 def toolTestingUpdate(className: str, neonId: int, inputDate: str):
-    date = datetime.datetime.strftime(datetime.datetime.strptime(inputDate, '%Y-%m-%d'), '%m/%d/%Y')
+    date = datetime.datetime.strftime(
+        datetime.datetime.strptime(inputDate, "%Y-%m-%d"), "%m/%d/%Y"
+    )
     fieldId = None
     for name, id in EVENT_FIELDS.items():
         if name in className:
             fieldId = id
             shortName = name
 
-    acctCustFields = neon.getAccountIndividual(neonId)['individualAccount']['accountCustomFields']
+    acctCustFields = neon.getAccountIndividual(neonId)["individualAccount"][
+        "accountCustomFields"
+    ]
 
     customIdList = []
     for field in acctCustFields:
-        customIdList.append(field['id'])
+        customIdList.append(field["id"])
     if fieldId and fieldId not in customIdList:
         try:
             ##### NEON #####
             # Update part of an account
             # https://developer.neoncrm.com/api-v2/#/Accounts/patchAccount
-            httpVerb = 'PATCH'
-            resourcePath = f'/accounts/{neonId}'
-            queryParams = ''
-            data = f'''
+            httpVerb = "PATCH"
+            resourcePath = f"/accounts/{neonId}"
+            queryParams = ""
+            data = f"""
             {{
             "individualAccount": {{
                 "accountCustomFields": [
@@ -79,24 +87,30 @@ def toolTestingUpdate(className: str, neonId: int, inputDate: str):
                 ]
             }}
             }}
-            '''
+            """
             url = N_baseURL + resourcePath + queryParams
 
             patch = apiCall(httpVerb, url, data, N_headers)
-            if (patch.status_code == 200):
-                logging.info(f"{patch.status_code} SUCCESS!  \n\tAccount ID {neonId} \n\tClass '{shortName}'")
+            if patch.status_code == 200:
+                logging.info(
+                    f"{patch.status_code} SUCCESS!  \n\tAccount ID {neonId} \n\tClass '{shortName}'"
+                )
             else:
-                logging.error(f"{patch.status_code} FAILED!  \n\tAccount ID {neonId} \n\tClass '{shortName}'")
+                logging.error(
+                    f"{patch.status_code} FAILED!  \n\tAccount ID {neonId} \n\tClass '{shortName}'"
+                )
 
         except:
-            logging.error(f"UPDATE FAILED FOR UNKNOWN REASON!  \n\tAccount ID {neonId} \n\tClass '{shortName}'")
+            logging.error(
+                f"UPDATE FAILED FOR UNKNOWN REASON!  \n\tAccount ID {neonId} \n\tClass '{shortName}'"
+            )
     elif not fieldId:
-        logging.info(f'{className} does not have a corresponding custom field')
+        logging.info(f"{className} does not have a corresponding custom field")
     else:
-        logging.debug(f'Account ID {neonId} already has {shortName} marked')
+        logging.debug(f"Account ID {neonId} already has {shortName} marked")
 
 
-searchFields = f'''
+searchFields = f"""
 [
     {{
         "field": "Event End Date",
@@ -114,38 +128,38 @@ searchFields = f'''
         "value": "No"
     }}
 ]
-'''
-outputFields = '''
+"""
+outputFields = """
 [
     "Event Name", 
     "Event ID",
     "Event End Date"
 ]
-'''
-logging.info(f'Starting Tool Testing update for {today}:')
+"""
+logging.info(f"Starting Tool Testing update for {today}:")
 
 try:
     eventSearch = neon.postEventSearch(searchFields, outputFields)
-    if (responseEvents := eventSearch['searchResults']):
+    if responseEvents := eventSearch["searchResults"]:
         for event in responseEvents:
-            eventName = event['Event Name']
-            eventId = event['Event ID']
-            eventDate = event['Event End Date']
-            registrants = neon.getEventRegistrants(eventId)['eventRegistrations']
-            if type(registrants) is not type(None): 
+            eventName = event["Event Name"]
+            eventId = event["Event ID"]
+            eventDate = event["Event End Date"]
+            registrants = neon.getEventRegistrants(eventId)["eventRegistrations"]
+            if type(registrants) is not type(None):
                 for registrant in registrants:
-                    attended = registrant["tickets"][0]["attendees"][0]["markedAttended"]
+                    attended = registrant["tickets"][0]["attendees"][0][
+                        "markedAttended"
+                    ]
                     if attended == True:
-                        toolTestingUpdate(eventName, registrant['registrantAccountId'], eventDate)
+                        toolTestingUpdate(
+                            eventName, registrant["registrantAccountId"], eventDate
+                        )
     else:
-        logging.info('Event Search contained no results')
+        logging.info("Event Search contained no results")
 except TypeError:
-    pass 
+    pass
 except:
-    logging.error('Event Search Failed')
+    logging.error("Event Search Failed")
     if traceback.format_exc():
         logging.error(traceback.format_exc())
-
-
-
-                    
