99a2f2679bfc8e6f6655ca9e8f033cb4aa3ae686
Matthew Miller <matthew.miller@asmbly.org>
2024-06-10 16:27:07 -0500

    refactor: use send_message instead of sendmail in gmail helper function

--- helpers/gmail.py
+++ helpers/gmail.py
@@ -1,22 +1,33 @@
-import smtplib, ssl, logging
+import smtplib
+import ssl
+import logging
+
+from email.mime.multipart import MIMEMultipart
 
 from config import G_user, G_password
+
 
 #################################################################################
 # Sent a MIME email object to its recipient using GMail
 #################################################################################
-def sendMIMEmessage(MIMEmessage):
-    if not "@" in MIMEmessage['To']:
+def sendMIMEmessage(MIMEmessage: MIMEMultipart):
+    if not "@" in MIMEmessage["To"]:
         raise ValueError("Message doesn't have a sane destination address")
 
-    MIMEmessage['From'] = "Asmbly AdminBot"
+    MIMEmessage["From"] = "Asmbly AdminBot"
 
-    logging.debug(f'''Sending email subject "{MIMEmessage['Subject']}" to {MIMEmessage['To']}''')
+    logging.debug(
+        "Sending email subject '%s' to %s", MIMEmessage["Subject"], MIMEmessage["To"]
+    )
 
     context = ssl.create_default_context()
     try:
         with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
             server.login(G_user, G_password)
-            server.sendmail(G_user, MIMEmessage['To'], MIMEmessage.as_string())
+            server.send_message(MIMEmessage, G_user)
     except:
-        logging.exception(f'''Failed sending email subject "{MIMEmessage['Subject']}" to {MIMEmessage['To']}''')
+        logging.exception(
+            "Failed sending email subject '%s' to %s",
+            MIMEmessage["Subject"],
+            MIMEmessage["To"],
+        )
