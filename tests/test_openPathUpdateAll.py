20381bfb97c925d4f378c0599b2c79b4239c918f
Cam <camherringshaw@gmail.com>
2025-08-31 12:48:46 -0500

    Add comments to explain the test

--- tests/test_openPathUpdateAll.py
+++ tests/test_openPathUpdateAll.py
@@ -17,10 +17,10 @@ class TestOpenPathUpdateAll:
         for i, groups in enumerate(group_lists):
             # Cannot have a user with id 0
             acct = (MockAltaUserBuilder()
-                                 .with_id(i+1)
-                                 .with_groups(groups)
-                                 .build()
-                                )
+                    .with_id(i+1)
+                    .with_groups(groups)
+                    .build()
+                   )
             accts[acct['OpenPathID']] = acct
         return accts
 
@@ -28,8 +28,8 @@ class TestOpenPathUpdateAll:
         accts = {}
         for aid in alta_ids:
             acct = (MockNeonUserBuilder()
-                              .with_alta_id(aid)
-                              .build())
+                    .with_alta_id(aid)
+                    .build())
             accts.update({acct['id']: acct})
         return accts
 
@@ -48,16 +48,28 @@ class TestOpenPathUpdateAll:
         }
 
     def test_update_all(self, mocker, setup_mocks):
+        # Create a set of fake accounts in Neon and Alta
         test_groups = (['test_group'], ['test_group_2'])
         (alta_accounts, neon_accounts) = self._create_matching_alta_and_neon_accounts(test_groups)
 
+        # When `openPathUtil.getAllUsers()` is called, return our fake accounts
+        # instead of actually connecting to OpenPath/Alta.
         setup_mocks['getAllUsers'].return_value = alta_accounts
 
+        # Based on the fake user accounts we've just created, figure out what
+        # arguments should be passed in when `openPathUtil.updateGroups` gets
+        # called in openPathUpdateAll.py. We'll then check these arguments
+        # against what actually gets passed in when the openPathUpdateAll
+        # script runs.
         expected_calls = [
             mocker.call(neon_acct, openPathGroups=group_list)
             for neon_acct, group_list
             in zip(neon_accounts.values(), test_groups)
         ]
 
+        # Run the openPathUpdateAll script with our fake accounts.
         openPathUpdateAll.openPathUpdateAll(neon_accounts)
+
+        # Verify that openPathUtil.updateGroups was called with the correct
+        # arguments, based on our fake accounts.
         setup_mocks['updateGroups'].assert_has_calls(expected_calls)
